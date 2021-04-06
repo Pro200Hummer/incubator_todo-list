745a84cfa18f72bb55486e5159cb555d280670e1
Pro200Hummer <nikolaj4ik.pasha@yandex.ru>
2021-04-06 18:35:23 +0300

    test for the logic of adding a task + the function of adding a new task

--- src/reducers/tasks-reducer.ts
+++ src/reducers/tasks-reducer.ts
@@ -7,19 +7,25 @@ export type RemoveTaskActionCreator = {
     taskID: string
     todoListID: string
 }
-
-export type AddTaskActionCreator ={
+export type AddTaskActionCreator = {
     type: "ADD-TASK"
     title: string
     todoListID: string
 }
+export type ChangeTaskStatusActionCreator = {
+    type: "CHANGE-TASK-STATUS"
+    taskID: string
+    isDone: boolean
+    todoListID: string
+}
 
 export type ActionType = RemoveTaskActionCreator
-|AddTaskActionCreator
+    | AddTaskActionCreator
+    | ChangeTaskStatusActionCreator
 
 
 export const tasksReducer = (state: TaskStateType, action: ActionType): TaskStateType => {
-    switch (action.type){
+    switch (action.type) {
         case "REMOVE-TASK":
             const todoListTasks = state[action.todoListID]
             state[action.todoListID] = todoListTasks.filter(task => task.id !== action.taskID)
@@ -33,15 +39,24 @@ export const tasksReducer = (state: TaskStateType, action: ActionType): TaskStat
             const allTodoListTasks = state[action.todoListID]
             state[action.todoListID] = [newTask, ...allTodoListTasks]
             return {...state}
+        case "CHANGE-TASK-STATUS":
+            const task: TaskType | undefined = state[action.todoListID].find(t => t.id === action.taskID)
+            if (task) {
+                task.isDone = action.isDone
+            }
+            return {...state}
         default:
             return state
     }
 }
 
 
-export const removeTaskAC = (taskID: string, todoListID: string):RemoveTaskActionCreator => {
+export const removeTaskAC = (taskID: string, todoListID: string): RemoveTaskActionCreator => {
     return {type: "REMOVE-TASK", taskID, todoListID}
 }
-export const addTaskAC = (title: string, todoListID: string):AddTaskActionCreator => {
-    return {type:"ADD-TASK", title, todoListID}
+export const addTaskAC = (title: string, todoListID: string): AddTaskActionCreator => {
+    return {type: "ADD-TASK", title, todoListID}
+}
+export const changeTaskStatusAC = (taskID: string, isDone: boolean, todoListID: string): ChangeTaskStatusActionCreator => {
+    return {type: "CHANGE-TASK-STATUS", taskID, isDone, todoListID}
 }
