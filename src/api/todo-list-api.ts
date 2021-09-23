c9d975075a537682bc81f6765076f47d29cb6d89
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-09-23 13:20:50 +0300

    Refactor into app API.

--- src/api/todo-list-api.ts
+++ src/api/todo-list-api.ts
@@ -2,16 +2,33 @@ import axios from "axios";
 import {TaskStatuses, TaskType, TodoTaskPriority} from "../features/TodoList/Task/tasks-reducer";
 import {TodoListType} from "../features/TodoList/todolist-reducer";
 
-/* Instance to requests */
+export type TaskAPIType = {
+    items: TaskType[]
+};
+
+export type ResponseType<D = {}> = {
+    "data": D
+    "messages": string[]
+    "resultCode": number
+};
+
+export type UpdateTaskRequestType = {
+    title?: string
+    description?: string | null
+    status?: TaskStatuses
+    priority?: TodoTaskPriority
+    startDate?: string | null
+    deadline?: string | null
+};
+
 export const instance = axios.create({
     baseURL: `https://social-network.samuraijs.com/api/1.1/`,
     withCredentials: true,
     headers: {
         'API-key': 'c3ff16a4-4b9d-490a-b188-2440deac59e8'
     }
-})
+});
 
-/* API */
 export const todoListApi = {
     createTodoList(todoListTitle: string) {
         return instance.post<ResponseType<{ item: TodoListType }>>(`todo-lists`, {
@@ -44,25 +61,4 @@ export const todoListApi = {
     updateTask(todoListID: string, taskID: string, updateTaskRequest: UpdateTaskRequestType) {
         return instance.put<ResponseType>(`todo-lists/${ todoListID }/tasks/${ taskID }`, updateTaskRequest)
     }
-}
-
-/* Types */
-
-export type TaskAPIType = {
-    items: TaskType[]
-}
-
-export type ResponseType<D = {}> = {
-    "data": D
-    "messages": string[]
-    "resultCode": number
-}
-
-export type UpdateTaskRequestType = {
-    title?: string
-    description?: string | null
-    status?: TaskStatuses
-    priority?: TodoTaskPriority
-    startDate?: string | null
-    deadline?: string | null
-}
+};
